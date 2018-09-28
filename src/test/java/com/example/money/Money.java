c70ed17da3e75ba1088e1d7953fd5ef969cf4aff
dhandy <dhandy@L-H5CG72463HV.PRCINS.NET>
2018-09-28 10:59:11 -0400

    Create tests for Dollar

---
+++ src/test/java/com/example/money/Money.java
@@ -0,0 +1,25 @@
+package com.example.money;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class Money {
+	@Nested
+	class MoneyTest {
+		@Test
+		public void testMultiplication() {
+			Dollar five = new Dollar(5);
+			assertEquals(new Dollar(10), five.times(2));
+			assertEquals(new Dollar(15), five.times(3));
+		}
+		@Test
+		public void testEquality() {
+			assertTrue(new Dollar(5).equals(new Dollar(5)));
+			assertFalse(new Dollar(5).equals(new Dollar(6)));
+		}
+	}
+}
