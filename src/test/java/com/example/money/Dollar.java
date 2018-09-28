9f74f91b49765bdb8a4ee91d81d3439b8ac075d3
dhandy <dhandy@L-H5CG72463HV.PRCINS.NET>
2018-09-28 10:59:34 -0400

    Implement Dollar

---
+++ src/test/java/com/example/money/Dollar.java
@@ -0,0 +1,16 @@
+package com.example.money;
+
+public class Dollar {
+	private int amount;
+	Dollar(int amount){
+		this.amount = amount;
+	}
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+	
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar) object;
+		return amount == dollar.amount;
+	}
+}
